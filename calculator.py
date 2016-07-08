e28bb819d7eed2032b7d5632c71c10ba7040d49e
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 18:05:31 -0700

    Created loop to take inputs of multiple lengths and added square function

--- calculator.py
+++ calculator.py
@@ -11,28 +11,36 @@ from arithmetic import *
 # Your code goes here
 def calculator(input_string):
 
-    tokens = input_string.split(" ")
-    #print tokens
-    num1_int = int(tokens[1])
-    num2_int = int(tokens[2]) or None
-    #print num1_int, num2_int
+    split_input_string = input_string.split(" ")
+    math_operation = split_input_string.pop(0)  
+    #print split_input_string
+    #print math_operation     
+    
 
-    if tokens[0] == "q":
+    for i in range(len(split_input_string)):
+        split_input_string[i] = int(split_input_string[i])
+        
+     
+        
+    if math_operation == "q":
         return
-    elif tokens[0] == "+":
-        added_numbers = add(num1_int, num2_int)
+    elif math_operation == "+":
+        added_numbers = add(split_input_string[0], split_input_string[1])
         print added_numbers
-    elif tokens[0] == "-":
-        subtracted_numbers = subtract(num1_int,num2_int)
+    elif math_operation == "-":
+        subtracted_numbers = subtract(split_input_string[0],split_input_string[1])
         print subtracted_numbers
-    elif tokens[0] == "*":
-        multiplied_numbers = multiply(num1_int,num2_int)
+    elif math_operation == "*":
+        multiplied_numbers = multiply(split_input_string[0],split_input_string[1])
         print multiplied_numbers
-    elif tokens[0] == "/":
-        divided_numbers = divide(num1_int,num2_int)
+    elif math_operation == "/":
+        divided_numbers = divide(split_input_string[0],split_input_string[1])
         print divided_numbers
-    elif tokens[0] == "square":
-        squared_numbers = square(num1_int)
+    elif math_operation == "square":
+        squared_numbers = square(split_input_string[0])
         print squared_numbers
 
-calculator("square 5")
+calculator("/ 6 3")
+#To Do: Add remaining math functions (cube, power, mod), comment entire file, add Docstring
+#Enable user to type input string directly in terminal through arithmetic.py
+#Enable user to input multiple arguments
